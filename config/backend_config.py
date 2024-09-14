471b7da652fad55a9546596dec33dc1f373a3ef5
ericbarber <eric.a.barber@gmail.com>
2024-09-13 23:38:34 -0700

    Add mount for delta table as backend

---
+++ config/backend_config.py
@@ -0,0 +1,9 @@
+# # Table paths for all Delta tables
+table_paths = {
+    "departments": "/mnt/delta/departments",
+    "features": "/mnt/delta/features",
+    "controls": "/mnt/delta/control_analysis",
+    "control_runs": "/mnt/delta/control_analysis",
+    "signal_detection": "/mnt/delta/signal_detection",
+    "notifications": "/mnt/delta/notifications"
+}
